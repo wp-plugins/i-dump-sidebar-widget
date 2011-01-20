c94956d9ee2de72061e228041c12b90cdcd34099
jswebsupport <jswebsupport@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-20 18:29:11 +0000

    git-svn-id: https://plugins.svn.wordpress.org/i-dump-sidebar-widget/tags/1.1.2@335237 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ i-dump-sidebar-widget.php
@@ -0,0 +1,175 @@
+<?php
+/**
+ * Plugin Name: i-Dump Sidebar Widget
+ * Plugin URI: http://www.webdesign-support.nl/plugins/i-dump-sidebar-widget
+ * Description: The i-Dump Sidebar Widget will show down your uploaded photos made with your iPhone in the sidebar. Of course you can setup your desired settings for the Widget. For this plugin you need the WP-Dump application, available in the AppStore on the iPhone and the i-Dump plugin.
+ * Version: 1.1.2
+ * Author: Joey Schuurbiers
+ * Author URI: http://www.webdesign-support.nl
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+/**
+ * Add function to widgets_init that'll load our widget.
+ * @since 0.1
+ */
+add_action( 'widgets_init', 'idump_load_widgets' );
+
+/**
+ * Register our widget.
+ * 'i-Dump_Sidebar_Widget' is the widget class used below.
+ *
+ * @since 0.1
+ */
+function idump_load_widgets() {
+	register_widget( 'iDump_Sidebar_Widget' );
+}
+
+function my_css() {
+    echo '<link type="text/css" rel="stylesheet" href="' . get_bloginfo('wpurl') .'/wp-content/plugins/i-dump-sidebar-widget/style.css" />' . "\n";
+}
+
+add_action('wp_head', 'my_css');
+
+/**
+ * i-Dump Sidebar Widget class.
+ * This class handles everything that needs to be handled with the widget:
+ * the settings, form, display, and update.  Nice!
+ *
+ * @since 0.1
+ */
+class iDump_Sidebar_Widget extends WP_Widget {
+
+	/**
+	 * Widget setup.
+	 */
+	function iDump_Sidebar_Widget() {
+		/* Widget settings. */
+		$widget_ops = array( 'classname' => 'idump-sidebar-widget', 'description' => __('You need the i-Dump plugin before you can let this widget work.', 'example') );
+
+		/* Widget control settings. */
+		$control_ops = array( 'width' => 250, 'height' => 350, 'id_base' => 'idump-sidebar-widget' );
+
+		/* Create the widget. */
+		$this->WP_Widget( 'idump-sidebar-widget', __('iDump Sidebar Widget', 'idump-sidebar-widget'), $widget_ops, $control_ops );
+	}
+
+	/**
+	 * How to display the widget on the screen.
+	 */
+	function widget( $args, $instance ) {
+		extract( $args );
+
+		/* Our variables from the widget settings. */
+		$title = apply_filters('widget_title', $instance['title'] );
+		$limit = $instance['limit'];
+
+		/* Before widget (defined by themes). */
+		echo $before_widget;
+
+		/* Display the widget title if one was input (before and after defined by themes). */
+		if ( $title )
+			echo $before_title . $title . $after_title;
+
+        echo '<div class="idump-sidebar-widget">';
+
+        /* Check if the idump table is present and count the images */
+        //$querycount = "SELECT id FROM `iphoto`";
+        //$sqlcount = @mysql_query($querycount);
+        //$count = @mysql_num_rows($sqlcount);
+		
+        //if ( ! $count ) {
+        //   printf(__('<span style="color:red;"><strong>ERROR!</strong><br/><a class="error" target="_blank" href="http://wordpress.org/extend/plugins/i-dump-iphone-to-wordpress-photo-uploader">i-Dump iPhone Photo Uploader</a> is not installed or you have no pictures added yet.</span>'));
+        //} 
+
+        $query = "SELECT * FROM `iphoto` ORDER BY `id` DESC LIMIT " . $limit;
+        $sql = mysql_query($query);
+		
+        while ($record = mysql_fetch_object($sql)){
+
+           // $time = date('d-m-Y', $record->date);
+            $blog_url = get_bloginfo('wpurl');
+            $file = $blog_url . '/wp-content/plugins/i-dump-iphone-to-wordpress-photo-uploader/files/' . $record->file; 
+            $filethumb = $blog_url . '/wp-content/plugins/i-dump-iphone-to-wordpress-photo-uploader/files/thumb/' . $record->file;
+            $width = $instance['width'];
+            $height = $instance['height'];
+                        
+            echo '<a href="' . $file . '"><img src="' . $filethumb . '" alt="' . $time . '" width="' . $width . '" height="' . $height . '" /></a>';
+        }
+  
+        echo '</div>';
+        
+     //   printf( '<div class="link-to-album"><a href="' . $idumpurl . '">' . __('View all %1$s photos', 'count') . '</a></div>', $count );
+
+		/* After widget (defined by themes). */
+		echo $after_widget;
+	}
+
+	/**
+	 * Update the widget settings.
+	 */
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+
+		/* Strip tags for title and name to remove HTML (important for text inputs). */
+		$instance['title'] = strip_tags( $new_instance['title'] );
+		$instance['limit'] = strip_tags( $new_instance['limit'] );
+        $instance['height'] = strip_tags( $new_instance['height'] );
+        $instance['width'] = strip_tags( $new_instance['width'] );
+
+		return $instance;
+	}
+
+	/**
+	 * Displays the widget settings controls on the widget panel.
+	 * Make use of the get_field_id() and get_field_name() function
+	 * when creating your form elements. This handles the confusing stuff.
+	 */
+	function form( $instance ) {
+
+        ?>
+        
+        <?php
+		/* Set up some default widget settings. */
+		$defaults = array( 'title' => __('i-Dump Sidebar Widget', 'title'), 'limit' => __('4', 'limit'), 'width' => __('80', 'width'), 'height' => __('80','height') );
+		$instance = wp_parse_args( (array) $instance, $defaults ); ?>
+
+		<div class="extra">
+            <small><a target="_blank" href="http://itunes.apple.com/us/app/wp-dump/id413231620?mt=8&ls=1">Do you already have the WP-Dump application on your iPhone?</a></small> 
+        </div>
+		<br/>
+        <div class="title">
+            <label for="widget-recent-posts-__i__-title">Title:</label> 
+        	<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo $instance['title']; ?>" />
+        </div>
+		<br/>
+		<div class="limit">
+            <label for="widget-recent-posts-__i__-number">Number of photos to show:</label><br /> 
+    		<input id="<?php echo $this->get_field_id( 'limit' ); ?>" name="<?php echo $this->get_field_name( 'limit' ); ?>" type="text" value="<?php echo $instance['limit']; ?>" size="3" />
+        </div>
+		<br/>
+		<div class="size">
+            <label for="widget-recent-posts-__i__-number">Width:</label><br /> 
+    		<input id="<?php echo $this->get_field_id( 'width' ); ?>" name="<?php echo $this->get_field_name( 'width' ); ?>" type="text" value="<?php echo $instance['width']; ?>" size="3" />
+        </div>
+		<br/>
+		<div class="size">
+            <label for="widget-recent-posts-__i__-number">Height:</label><br /> 
+    		<input id="<?php echo $this->get_field_id( 'height' ); ?>" name="<?php echo $this->get_field_name( 'height' ); ?>" type="text" value="<?php echo $instance['height']; ?>" size="3" />
+        </div>  
+        
+        <?php
+		//$querycount = "SELECT id FROM `iphoto`";
+		//$sqlcount = mysql_query($querycount) or die ( mysql_error( ) );
+		//$count = mysql_num_rows($sqlcount);
+        ?>
+  
+        <!--<small>There are <?//=$count;?> pictures in your database.</small>-->
+
+	<?php
+	}
+  }
+?>
---
+++ readme.txt
@@ -0,0 +1,70 @@
+=== i-Dump Sidebar Widget ===
+Contributors: Joey Schuurbiers
+Donate link: http://www.i-dump.info
+Tags: idump,i-dump,idump widget,sidebar widget,sidebar,widget,gallery,photos,wpdump,dump,mobile,iphone
+Requires at least: 3.0
+Tested up to: 3.0
+Stable tag: 1.1.2
+
+The i-Dump Sidebar Widget will show down your uploaded photos made with your iPhone in the sidebar.
+
+== Description ==
+
+The i-Dump Sidebar Widget will show down your uploaded photos made with your iPhone in the sidebar. Of course you can setup your desired settings for the Widget. For this plugin you need the WP-Dump application, available in the AppStore on the iPhone and the i-Dump plugin.
+
+== Installation ==
+
+1. Upload the `i-dump-sidebar-widget` folder to the `/wp-content/plugins/` directory.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+3. Go to 'Appearance' -> 'Widgets' into your WordPress Menu, and see the new 'i-Dump Sidebar Widget' widget.
+4. Setup your desired settings, so how many pictures you want to show down into your sidebar and what will be the 'width' and 'height' for all pictures.
+5. <strong>Caution:</strong> Install the <a href="http://wordpress.org/extend/plugins/i-dump-iphone-to-wordpress-photo-uploader/">i-Dump plugin</a> for WordPress and download the <a href="http://itunes.apple.com/nl/app/wp-dump/id413231620?mt=8&ls=1">WP-Dump application</a> on your iPhone before you can run the i-Dump Sidebar Widget!
+
+== Frequently Asked Questions ==
+
+= My widget is not working? =
+
+That could be possible, because you will need to download the <a href="http://wordpress.org/extend/plugins/i-dump-iphone-to-wordpress-photo-uploader/">i-Dump plugin</a> first before you can run this widget.
+
+= What do I need to make the i-Dump Sidebar Widget work? =
+
+You will need 2 things for sure, and 1 thing as option.
+First of all you need an iPhone with the <a href="http://itunes.apple.com/nl/app/wp-dump/id413231620?mt=8&ls=1">WP-Dump application</a> on it (you can download that app in the AppStore).
+Then you will need the <a href="http://wordpress.org/extend/plugins/i-dump-iphone-to-wordpress-photo-uploader/">i-Dump plugin</a> as well, because the i-Dump Sidebar Widget is an expanding feature of the i-Dump plugin.
+Last but not least, it would be nice to have the <a href="http://wordpress.org/extend/plugins/easy-fancybox/">Easy Fancybox plugin</a>. That will show down your pictures perfectly!
+
+= When I click on a picture, it opens in the same window and looks bad? =
+
+Correct, the default settings for showing a picture are bad looking. We have a great solution for that: Download the <a href="http://wordpress.org/extend/plugins/easy-fancybox/">Easy Fancybox plugin</a> for a great view when you clicked on a picture.
+
+= I have another question? =
+
+For more information or suggestions you can <a href="mailto:development@webdesign-support.nl">contact the developer</a> of the i-Dump Sidebar Widget.
+
+== Screenshots ==
+
+1. The working i-Dump Sidebar Widget: Front-End
+2. The working i-Dump Sidebar Widget: Back-End
+
+== Changelog ==
+
+= Version 1.1.2 =
+* Bug fixed: When you don't have the i-Dump plugin, the i-Dump Sidebar Widget will disappear and your widget area will not crash as before.
+* This version of i-Dump Sidebar Widget, will be working correctly with the WP-Dump application for iPhone and the i-Dump plugin.
+The i-Dump plugin is a must have for running the i-Dump Sidebar Widget.
+
+= Version 1.0 =
+* This version of i-Dump Sidebar Widget, will be working correctly with the WP-Dump application for iPhone and the i-Dump plugin.
+The i-Dump plugin is a must have for running the i-Dump Sidebar Widget.
+
+== Upgrade Notice == 
+
+= Version 1.1.2 =
+* Bug fixed: When you don't have the i-Dump plugin, the i-Dump Sidebar Widget will disappear and your widget area will not crash as before.
+
+== About this Widget ==
+
+This version of i-Dump Sidebar Widget, will be working correctly with the WP-Dump application for iPhone and the i-Dump plugin.
+The i-Dump plugin is a must have for running the i-Dump Sidebar Widget.
+
+For more information or suggestions you can <a href="mailto:development@webdesign-support.nl">contact the developer</a> of the i-Dump Sidebar Widget.
